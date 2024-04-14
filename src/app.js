0ec10d74055c4d10ae9f3f6c552f24305ee484be
Gabriel Castro <gbrlcstr@hotmail.com>
2024-04-14 10:08:56 -0300

    estructura del proyecto

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const myconnection = require("express-myconnection");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = express();
+app.set("port", 8080)
+
+app.use(myconnection(mysql, {
+    host: "localhost",
+    user: "root",
+    password: "",
+    port: 3306,
+    database: "TODOLIST"
+}));
+
+app.set("views", __dirname + "/views");
+app.engine(".hbs", engine({
+    extname: ".hbs"
+}));
+app.set("view engine", "hbs");
+
+app.listen(app.get("port"), () => {
+    console.log("Server listening on port", app.get("port"))
+});
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
