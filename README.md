23352e64fd6b3146716f292712bdfcc327db71ef
Gabriel Castro <gbrlcstr@hotmail.com>
2024-04-14 13:45:46 -0300

    Se crea CRUD con MySQL y se estiliza la app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Aplicacion TodoList NodeJS - MySQL
+## CRUD con Javascript, NodeJS y MySQL. aplicacion TodoList donde podemos agregar tareas, visualizarlas, editarlas y eliminarlas.
+
++ Para desarrollar esta App se hizo uso de las librerias:
+    + Express : Permite inicializar un servidor.
+    + Express-Handlebars: Permite generar las vistas de nuestro proyecto.
+    + MySQL: Permite la creacion de las bases de datos relaciones.
+    + Express-MyConnection: Permite conectar el servidor con la base de datos.
+    + Body-Parser: Permite manejar los datos que proviene de las peticiones.
+    + Nodemon: Permite ver los cambios de la App en tiempo real.
--- src/app.js
+++ src/app.js
@@ -1,34 +1,38 @@
-const express = require("express");
-const { engine } = require("express-handlebars");
-const myconnection = require("express-myconnection");
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const tasksRoutes = require("./routes/tasks");
+const express = require('express');
+const { engine } = require('express-handlebars');
+const myconnection = require('express-myconnection');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const tasksRoutes = require('./routes/tasks');
 
 const app = express();
-app.set("port", 8080)
+app.set('port', 4000);
 
-app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('views', __dirname + '/views');
+app.engine('.hbs', engine({
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+
 app.use(myconnection(mysql, {
-    host: "localhost",
-    user: "root",
-    password: "",
-    port: 3306,
-    database: "TODOLIST"
-}));
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  port: 3306,
+  database: 'TODOLIST'
+}, 'single'));
 
-app.set("views", __dirname + "/views");
-app.engine(".hbs", engine({
-    extname: ".hbs"
-}));
-app.set("view engine", "hbs");
-
-app.listen(app.get("port"), () => {
-    console.log("Server listening on port", app.get("port"))
+app.listen(app.get('port'), () => {
+  console.log('Listening on port ', app.get('port'));
 });
 
-app.use("/", tasksRoutes);
+app.use('/', tasksRoutes);
 
-app.get("/", (req, res) => {
-    res.render("home");
-})
+app.get('/', (req, res) => {
+  res.render('home');
+});
